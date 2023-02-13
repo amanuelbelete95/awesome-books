614a8374821f827bd4f12465236a20efaeb6edfa
Amanuel Belete <amanuelbelete95@gmail.com>
2023-02-13 19:29:31 +0300

    Check the linter configuration

--- script.js
+++ script.js
@@ -3,38 +3,26 @@ const author = document.querySelector('#author');
 const add = document.querySelector('#add-btn');
 const bookContainer = document.querySelector('.book-container');
 
-
-add.addEventListener('click', () => {
-  populateStorage();
-
-});
-
-
-
-const bookArray = [];
-
 function setValue() {
   const currentstoreddata = localStorage.getItem('storeddata');
   const parseddata = JSON.parse(currentstoreddata);
 
-  
-
-  for (let i = 0; i < parseddata.length; i++) {
+  for (let i = 0; i < parseddata.length; i += 1) {
     const newBook = document.createElement('p');
     const newAuthor = document.createElement('p');
     const removeBtn = document.createElement('button');
-    const hr = document.createElement('hr')
+    const hr = document.createElement('hr');
 
     bookContainer.appendChild(newBook);
     bookContainer.appendChild(newAuthor);
     bookContainer.appendChild(removeBtn);
-    bookContainer.appendChild(hr); 
+    bookContainer.appendChild(hr);
 
     newBook.innerHTML = parseddata[i].book;
     newAuthor.innerHTML = parseddata[i].author;
     removeBtn.innerHTML = 'Remove';
 
-    removeBtn.addEventListener('click', ()=>{
+    removeBtn.addEventListener('click', () => {
       bookContainer.removeChild(newBook);
       bookContainer.removeChild(newAuthor);
       bookContainer.removeChild(removeBtn);
@@ -43,8 +31,6 @@ function setValue() {
       localStorage.setItem('storeddata', JSON.stringify(parseddata));
     });
   }
-
-  
 }
 
 function populateStorage() {
@@ -54,7 +40,7 @@ function populateStorage() {
   let stringifydata = '';
   bookData.book = titleInput.value;
   bookData.author = author.value;
-  
+
   titleInput.value = '';
   author.value = '';
   bookArray.push(bookData);
@@ -64,9 +50,10 @@ function populateStorage() {
   setValue();
 }
 
+add.addEventListener('click', () => {
+  populateStorage();
+});
+
 if (localStorage.getItem('storeddata')) {
   setValue();
-} 
-
-
-
+}
