3c7d17717d6724c6d4ea74c51b7ee07c13cf9100
isaacg <iwanyoike65@gmail.com>
2023-02-15 18:46:19 +0300

    Add nav.js to script.js/fix bugs

--- script.js
+++ script.js
@@ -109,3 +109,38 @@ const time = date.toLocaleString('en-US', {
 });
 
 dateElement.textContent = `${month} ${day} ${year}, ${time}`;
+
+const list = document.querySelector('.nav-list');
+const add1 = document.querySelector('.nav-add');
+const contact = document.querySelector('.nav-contact');
+
+const listBook = document.querySelector('.list-books');
+const form = document.querySelector('.form');
+const contactSection = document.querySelector('.contact');
+
+list.addEventListener('click', () => {
+  form.style.display = 'none';
+  contactSection.style.display = 'none';
+  listBook.style.display = 'flex';
+  list.style.color = 'blue';
+  add1.style.color = 'black';
+  contact.style.color = 'black';
+});
+
+add1.addEventListener('click', () => {
+  form.style.display = 'flex';
+  contactSection.style.display = 'none';
+  listBook.style.display = 'none';
+  add1.style.color = 'blue';
+  list.style.color = 'black';
+  contact.style.color = 'black';
+});
+
+contact.addEventListener('click', () => {
+  form.style.display = 'none';
+  contactSection.style.display = 'flex';
+  listBook.style.display = 'none';
+  contact.style.color = 'blue';
+  add1.style.color = 'black';
+  list.style.color = 'black';
+});
