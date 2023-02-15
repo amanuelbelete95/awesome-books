b31c83e27a9c023266b10066ac4c6e5f3bd53e8b
Amanuel Belete <amanuelbelete95@gmail.com>
2023-02-15 18:16:22 +0300

    Check linter errors

--- nav.js
+++ nav.js
@@ -6,30 +6,29 @@ const listBook = document.querySelector('.list-books');
 const form = document.querySelector('.form');
 const contactSection = document.querySelector('.contact');
 
-list.addEventListener('click', ()=>{
+list.addEventListener('click', () => {
   form.style.display = 'none';
   contactSection.style.display = 'none';
   listBook.style.display = 'flex';
   list.style.color = 'blue';
   add.style.color = 'black';
   contact.style.color = 'black';
-})
+});
 
-add.addEventListener('click', ()=>{
+add.addEventListener('click', () => {
   form.style.display = 'flex';
   contactSection.style.display = 'none';
   listBook.style.display = 'none';
   add.style.color = 'blue';
   list.style.color = 'black';
   contact.style.color = 'black';
-})
+});
 
-contact.addEventListener('click', ()=>{
-
+contact.addEventListener('click', () => {
   form.style.display = 'none';
   contactSection.style.display = 'flex';
   listBook.style.display = 'none';
   contact.style.color = 'blue';
   list.style.color = 'black';
   list.style.color = 'black';
-})
+});
--- script.js
+++ script.js
@@ -98,14 +98,14 @@ if (localStorage.getItem('storeddata')) {
   setValue(library2);
 }
 
-
-const dateElement = document.getElementById("date");
+const dateElement = document.getElementById('date');
 const date = new Date();
 
 const month = date.toLocaleString('default', { month: 'long' });
 const day = date.getDate();
 const year = date.getFullYear();
-const time = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+const time = date.toLocaleString('en-US', {
+  hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true,
+});
 
 dateElement.textContent = `${month} ${day} ${year}, ${time}`;
-
